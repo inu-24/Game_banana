136c8d7b3fc1d7b914af0434daec0d855a1b87df
Inuli Gunasekara <gunasekarainuli@gmail.com>
2026-03-10 12:57:08 +0530

    Adding update password file

---
+++ update_password.php
@@ -0,0 +1,25 @@
+<?php
+include("db.php");
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+$token=$_POST['token'];
+$password=$_POST['password'];
+
+$hashed = password_hash($password, PASSWORD_DEFAULT);
+
+$sql="UPDATE users 
+SET password=?, reset_token=NULL, reset_expiry=NULL 
+WHERE reset_token=?";
+
+$stmt=$conn->prepare($sql);
+$stmt->bind_param("ss",$hashed,$token);
+
+if($stmt->execute()){
+echo "<script>alert('Password updated successfully'); window.location='login.html';</script>";
+}else{
+echo "Error updating password";
+}
+
+}
+?>
